1defd417450f5f328c403cd2c599308be403aff6
Asiimire Patricia <101188715+asiimire@users.noreply.github.com>
2024-08-31 16:50:55 +0300

    Update settings.py

--- seechat/settings.py
+++ seechat/settings.py
@@ -1,186 +1,20 @@
-# from pathlib import Path
-
-# from dotenv import load_dotenv
-# import os
-# import dj_database_url
-# load_dotenv()
-
-# # DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-
-# # AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
-# # AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
-# # AWS_STORAGE_BUCKET_NAME = os.getenv('AWS_STORAGE_BUCKET_NAME')
-# # AWS_S3_REGION_NAME = os.getenv('AWS_S3_REGION_NAME', 'eu-north-1')
-# # AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
-# # AWS_S3_FILE_OVERWRITE = False
-# # AWS_DEFAULT_ACL = None
-# # AWS_LOCATION = 'media'
-
-
-# # Build paths inside the project like this: BASE_DIR / 'subdir'.
-# BASE_DIR = Path(__file__).resolve().parent.parent
-
-
-# # Quick-start development settings - unsuitable for production
-# # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
-
-# # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'django-insecure-w52qapoq5!^4qo2-!!rr#6_1#-(hhblax2#^4idkm97x5$3q+6'
-# GOOGLE_APPLICATION_CREDENTIALS = os.getenv('GOOGLE_APPLICATION_CREDENTIALS')
-# from django.core.management.utils import get_random_secret_key
-
-# SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', default=get_random_secret_key())
-# DEBUG = True
-# # SECURITY WARNING: don't run with debug turned on in production!
-
-
-# ALLOWED_HOSTS = ["*"]
-
-
-# # Application definition
-
-# INSTALLED_APPS = [
-#     'django.contrib.admin',
-#     'django.contrib.auth',
-#     'django.contrib.contenttypes',
-#     'django.contrib.sessions',
-#     'django.contrib.messages',
-#     'django.contrib.staticfiles',
-#     'base',
-#     'storages',
-#     'crispy_forms',
-#     'crispy_bootstrap5',
-# ]
-
-# CRISPY_TEMPLATE_PACK = 'bootstrap5'
-
-# # # Backblaze B2 settings
-# # DEFAULT_FILE_STORAGE = 'storages.backends.b2.B2Storage'
-# # B2_BUCKET_NAME = 'galiniplus'
-# # B2_ACCOUNT_ID = '005188e9b386f400000000001'  # This is your keyID
-# # B2_APPLICATION_KEY = 'K005Le9LbTFWewwvjhJVU89IpBKZHDY'  # This is your applicationKey
-# # # B2_ACCOUNT_ID = os.getenv('B2_ACCOUNT_ID')
-# # # B2_APPLICATION_KEY = os.getenv('B2_APPLICATION_KEY')
-# # # Optional: Set custom domain or path to files
-
-# MIDDLEWARE = [
-#     'django.middleware.security.SecurityMiddleware',
-#     "whitenoise.middleware.WhiteNoiseMiddleware",
-#     'django.contrib.sessions.middleware.SessionMiddleware',
-#     'django.middleware.common.CommonMiddleware',
-#     'django.middleware.csrf.CsrfViewMiddleware',
-#     'django.contrib.auth.middleware.AuthenticationMiddleware',
-#     'django.contrib.messages.middleware.MessageMiddleware',
-#     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-# ]
-
-
-# ROOT_URLCONF = 'seechat.urls'
-
-# TEMPLATES = [
-#     {
-#         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-#         'DIRS': [],
-#         'APP_DIRS': True,
-#         'OPTIONS': {
-#             'context_processors': [
-#                 'django.template.context_processors.debug',
-#                 'django.template.context_processors.request',
-#                 'django.contrib.auth.context_processors.auth',
-#                 'django.contrib.messages.context_processors.messages',
-#             ],
-#         },
-#     },
-# ]
-
-# WSGI_APPLICATION = 'seechat.wsgi.application'
-
-
-# # Database
-# # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
-
-
-# DATABASES = {
-#     'default': dj_database_url.config(
-#         default=os.getenv('DATABASE_URL')
-#     )
-# }
-
-# # Password validation
-# # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
-
-# AUTH_PASSWORD_VALIDATORS = [
-#     {
-#         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-#     },
-#     {
-#         'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-#     },
-#     {
-#         'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-#     },
-#     {
-#         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-#     },
-# ]
-
-
-# # Internationalization
-# # https://docs.djangoproject.com/en/5.0/topics/i18n/
-
-# LANGUAGE_CODE = 'en-us'
-
-# TIME_ZONE = 'UTC'
-
-# USE_I18N = True
-
-# USE_TZ = True
-
-
-# # Static files (CSS, JavaScript, Images)
-# # https://docs.djangoproject.com/en/5.0/howto/static-files/
-
-# STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
-# # STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-# STATIC_URL = 'static/'
-# # STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/static/'
-# STATICFILES_DIRS = [
-#     BASE_DIR /'static',
-# ]
-# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
-# MEDIA_URL = 'media/'
-# # MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{AWS_LOCATION}/'
-# # MEDIA_URL = f'https://f002.backblazeb2.com/file/{B2_BUCKET_NAME}/'
-
-# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
-# # send email notification
-# # https://myaccount.google.com/lesssecureapps
-# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-# EMAIL_HOST = os.getenv('EMAIL_HOST','smtp.gmail.com')
-# EMAIL_PORT = 587
-# EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
-# EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
-# EMAIL_USE_TLS = True
-# # EMAIL_USE_SSL = True
-# GOOGLE_TRANSLATE_API_KEY = os.getenv('GOOGLE_TRANSLATE_API_KEY')
-# # send sms notification
-# TWILIO_ACCOUNT_SID = os.getenv('TWILIO_ACCOUNT_SID')
-# TWILIO_AUTH_TOKEN = os.getenv('TWILIO_AUTH_TOKEN')
-# TWILIO_PHONE_NUMBER = os.getenv('TWILIO_PHONE_NUMBER')
-# SMS_BROADCAST_TO_NUMBERS = os.getenv('SMS_BROADCAST_TO_NUMBERS', '').split(',')
-
-# # Default primary key field type
-# # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
-
-# DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-
-
-
 from pathlib import Path
+
+from dotenv import load_dotenv
 import os
+import dj_database_url
+load_dotenv()
+
+# DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+# AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
+# AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
+# AWS_STORAGE_BUCKET_NAME = os.getenv('AWS_STORAGE_BUCKET_NAME')
+# AWS_S3_REGION_NAME = os.getenv('AWS_S3_REGION_NAME', 'eu-north-1')
+# AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+# AWS_S3_FILE_OVERWRITE = False
+# AWS_DEFAULT_ACL = None
+# AWS_LOCATION = 'media'
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -191,12 +25,16 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-rjp02#8fqr17a&n)h*4ihz&=xnf+5+*8p+dnf5q++q6_(db4xa'
+SECRET_KEY = 'django-insecure-w52qapoq5!^4qo2-!!rr#6_1#-(hhblax2#^4idkm97x5$3q+6'
+GOOGLE_APPLICATION_CREDENTIALS = os.getenv('GOOGLE_APPLICATION_CREDENTIALS')
+from django.core.management.utils import get_random_secret_key
 
-# SECURITY WARNING: don't run with debug turned on in production!
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', default=get_random_secret_key())
 DEBUG = True
+# SECURITY WARNING: don't run with debug turned on in production!
 
-ALLOWED_HOSTS = []
+
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -208,13 +46,26 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'channels',
-    'mental',
-    
+    'base',
+    'storages',
+    'crispy_forms',
+    'crispy_bootstrap5',
 ]
+
+CRISPY_TEMPLATE_PACK = 'bootstrap5'
+
+# # Backblaze B2 settings
+# DEFAULT_FILE_STORAGE = 'storages.backends.b2.B2Storage'
+# B2_BUCKET_NAME = 'galiniplus'
+# B2_ACCOUNT_ID = '005188e9b386f400000000001'  # This is your keyID
+# B2_APPLICATION_KEY = 'K005Le9LbTFWewwvjhJVU89IpBKZHDY'  # This is your applicationKey
+# # B2_ACCOUNT_ID = os.getenv('B2_ACCOUNT_ID')
+# # B2_APPLICATION_KEY = os.getenv('B2_APPLICATION_KEY')
+# # Optional: Set custom domain or path to files
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -223,12 +74,13 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'galiniapp.urls'
+
+ROOT_URLCONF = 'seechat.urls'
 
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR/'templates'],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -236,32 +88,23 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                # 'therapists.context_processors.therapists',
             ],
         },
     },
 ]
 
-WSGI_APPLICATION = 'galiniapp.wsgi.application'
-# ASGI_APPLICATION = 'galiniapp.asgi.application'
-
-# CHANNEL_LAYERS = {
-#     'default': {
-#         'BACKEND': 'channels.layers.InMemoryChannelLayer',
-#     },
-# }
+WSGI_APPLICATION = 'seechat.wsgi.application'
 
 
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 
+DATABASES = {
+    'default': dj_database_url.config(
+        default=os.getenv('DATABASE_URL')
+    )
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
@@ -297,11 +140,36 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+# STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 STATIC_URL = 'static/'
-STATICFILES_DIRS = ['static/']
+# STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/static/'
+STATICFILES_DIRS = [
+    BASE_DIR /'static',
+]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-MEDIA_URL = 'MEDIA/'
+MEDIA_URL = 'media/'
+# MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{AWS_LOCATION}/'
+# MEDIA_URL = f'https://f002.backblazeb2.com/file/{B2_BUCKET_NAME}/'
+
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+# send email notification
+# https://myaccount.google.com/lesssecureapps
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = os.getenv('EMAIL_HOST','smtp.gmail.com')
+EMAIL_PORT = 587
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+EMAIL_USE_TLS = True
+# EMAIL_USE_SSL = True
+GOOGLE_TRANSLATE_API_KEY = os.getenv('GOOGLE_TRANSLATE_API_KEY')
+# send sms notification
+TWILIO_ACCOUNT_SID = os.getenv('TWILIO_ACCOUNT_SID')
+TWILIO_AUTH_TOKEN = os.getenv('TWILIO_AUTH_TOKEN')
+TWILIO_PHONE_NUMBER = os.getenv('TWILIO_PHONE_NUMBER')
+SMS_BROADCAST_TO_NUMBERS = os.getenv('SMS_BROADCAST_TO_NUMBERS', '').split(',')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
